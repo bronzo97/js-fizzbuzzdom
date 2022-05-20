d5c5ef5dd07afeb1c092c86ec69b82855696507f
bronzo97 <danilobronzo97@hotmail.it>
2022-05-20 15:58:23 +0200

    updates

--- js/app.js
+++ js/app.js
@@ -1,14 +1,22 @@
+const container = document.querySelector('[class="container"]');
+
 for (i = 1; i <= 100; i++) {
 
     if (i % 3 === 0 & i % 5 === 0) {
         console.log('FrizzBuzz');
+        container.innerHTML += `${'FrizzBuzz'}`;
     } else if (i % 3 === 0) {
         console.log('Frizz');
+        container.innerHTML += `${'Frizz'}`;
+
     } else if (i % 5 === 0) {
         console.log('Buzz');
-    }
-    else {
+        container.innerHTML += `${'Buzz'}`;
+
+    } else {
         console.log(i);
+        container.innerHTML += `${i}`;
+
     }
     
 }
