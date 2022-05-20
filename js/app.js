96ce463ec3ff9181bbbb3f8a1ff8104ef8bbf8eb
bronzo97 <danilobronzo97@hotmail.it>
2022-05-20 16:15:51 +0200

    updates

--- js/app.js
+++ js/app.js
@@ -1,21 +1,21 @@
-const container = document.querySelector('[class="container"]');
+const row = document.querySelector('[class="row gap-3"]');
 
 for (i = 1; i <= 100; i++) {
 
     if (i % 3 === 0 & i % 5 === 0) {
         console.log('FrizzBuzz');
-        container.innerHTML += `${'FrizzBuzz'}`;
+        row.innerHTML += `<div class="col frizz-buzz">${'FrizzBuzz'}</div>`;
     } else if (i % 3 === 0) {
         console.log('Frizz');
-        container.innerHTML += `${'Frizz'}`;
+        row.innerHTML += `<di class="col frizz">${'Frizz'}</di>`;
 
     } else if (i % 5 === 0) {
         console.log('Buzz');
-        container.innerHTML += `${'Buzz'}`;
+        row.innerHTML += `<div class="col buzz">${'Buzz'}</div>`;
 
     } else {
         console.log(i);
-        container.innerHTML += `${i}`;
+        row.innerHTML += `<div class="col number">${i}</div>`;
 
     }
     
